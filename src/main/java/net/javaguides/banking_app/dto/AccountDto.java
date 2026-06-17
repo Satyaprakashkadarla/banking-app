3e59cdcf1d6912390b2afd427b5445cfc246a9c8
Satyaprakashkadarla <satyaprakashkadarla@gmail.com>
2026-06-17 07:20:00 +0530

    Initial commit - Banking Application

---
+++ src/main/java/net/javaguides/banking_app/dto/AccountDto.java
@@ -0,0 +1,15 @@
+package net.javaguides.banking_app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountDto {
+
+    private Long id;
+    private String accountHolderName;
+    private double balance;
+}
---
+++ src/main/java/net/javaguides/banking_app/service/impl/AccountServiceImpl.java
@@ -0,0 +1,83 @@
+package net.javaguides.banking_app.service.impl;
+
+import net.javaguides.banking_app.dto.AccountDto;
+import net.javaguides.banking_app.entity.Account;
+import net.javaguides.banking_app.mapper.AccountMapper;
+import net.javaguides.banking_app.repository.AccountRepository;
+import net.javaguides.banking_app.service.AccountService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+
+    private final AccountRepository accountRepository;
+
+    public AccountServiceImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public AccountDto createAccount(AccountDto accountDto) {
+        Account account = AccountMapper.mapToAccount(accountDto);
+        Account savedAccount = accountRepository.save(account);
+        return AccountMapper.mapToAccountDto(savedAccount);
+    }
+
+    @Override
+    public AccountDto getAccountById(Long id) {
+        Account account = accountRepository
+                .findById(id)
+                .orElseThrow(() -> new RuntimeException("Account does not exist"));
+
+        return AccountMapper.mapToAccountDto(account);
+    }
+
+    @Override
+    public AccountDto deposit(Long id, double amount) {
+        Account account = accountRepository
+                .findById(id)
+                .orElseThrow(() -> new RuntimeException("Account does not exist"));
+
+        account.setBalance(account.getBalance() + amount);
+
+        Account savedAccount = accountRepository.save(account);
+        return AccountMapper.mapToAccountDto(savedAccount);
+    }
+
+    @Override
+    public AccountDto withdraw(Long id, double amount) {
+        Account account = accountRepository
+                .findById(id)
+                .orElseThrow(() -> new RuntimeException("Account does not exist"));
+
+        if (account.getBalance() < amount) {
+            throw new RuntimeException("Insufficient Amount");
+        }
+
+        account.setBalance(account.getBalance() - amount);
+
+        Account savedAccount = accountRepository.save(account);
+        return AccountMapper.mapToAccountDto(savedAccount);
+    }
+
+    @Override
+    public List<AccountDto> getAllAccounts() {
+        List<Account> accounts = accountRepository.findAll();
+
+        return accounts.stream()
+                .map(AccountMapper::mapToAccountDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void deleteAccount(Long id) {
+        Account account = accountRepository
+                .findById(id)
+                .orElseThrow(() -> new RuntimeException("Account does not exist"));
+
+        accountRepository.deleteById(id);
+    }
+}
